7faea516876cb870669b5fd8b1608c5293e60473
Shuvham <shuvham2003.sg@gmail.com>
2024-09-23 18:41:08 +0530

    create user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,16 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.database.db import Base
+
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    email = Column(String, unique=True, index=True)
+
+    tasks = relationship("Tasks", back_populates="owner")
