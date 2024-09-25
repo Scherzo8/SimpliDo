54655ff6838b23fc7e10241cf9695a4b65d02054
Shuvham <shuvham2003.sg@gmail.com>
2024-09-26 04:17:00 +0530

    Initialize FastAPI app with database setup and routers

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from app.database.db import Base, engine
+from app.routes import user, task
+from app.update_version import get_version
 
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title='Todo Application', 
               description='A complex to-do application with user authentication and task management.', 
-              version='0.1.0')
+              version=get_version())
 
-@app.get("/", summary="Root Endpoint")
+@app.include_router(user.router)
+@app.include_router(task.router)
+
+@app.get("/", summary="Root Endpoint", tags=["Root"])
 def home() -> dict: 
 
     return {"message": "Welcome to Simplido"}
